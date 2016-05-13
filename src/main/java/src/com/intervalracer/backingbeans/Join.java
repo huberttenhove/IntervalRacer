8a359bc71657b3ff46065558b43aeb01b20cb4aa
Hubert ten Hove <huberttenhove@gmail.com>
2016-05-14 01:55:48 +0200

    Terug naar de oude situatie, ajax test 325.61 werkt ook niet...

--- src/main/java/src/com/intervalracer/backingbeans/Join.java
+++ src/main/java/src/com/intervalracer/backingbeans/Join.java
@@ -54,10 +54,12 @@ public class Join {
 	}
 
 	/**
-	 * Accepts new commands from player.
+	 * Accepts new commands from player. Returns redirect=true so after reload a
+	 * command is not given again.
 	 */
-	public void setNewCommand(String command) {
+	public String setNewCommand(String command) {
 		playerManager.getPlayer().setLastCommand(command);
+		return "RaceView?faces-redirect=true";
 	}
 
 	/**
