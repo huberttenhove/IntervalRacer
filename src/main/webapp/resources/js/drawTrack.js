9e4429fcc84809c770af2a57c4bbf01ed9485cc8
Hubert ten Hove <huberttenhove@gmail.com>
2016-05-14 10:35:14 +0200

    Finishline and checkpoints now visual in browser

--- src/main/webapp/resources/js/drawTrack.js
+++ src/main/webapp/resources/js/drawTrack.js
@@ -1,21 +1,21 @@
 	function drawTrack(){
 		drawSoftCurveTrackElement(200, 200, 1);
-		drawHorizontalTrackElement(200, 0);
-		drawHorizontalTrackElement(400, 0);
+		drawHorizontalTrackElement(200, 0, "grey");
+		drawHorizontalTrackElement(400, 0, "darkgrey");
 		drawSoftCurveTrackElement(600, 200, 1.5);
 		drawSoftCurveTrackElement(600, 200, 0);
-		drawHorizontalTrackElement(400, 200);
+		drawHorizontalTrackElement(400, 200, "darkgrey");
 		drawSoftCurveTrackElement(400, 400, 0.5);
 		drawRectangle(200,400,200,200,"white");
 		drawSoftCurveTrackElement(200, 400, 0);
 		drawSoftCurveTrackElement(200, 400, 0.5);
-		drawVerticalTrackElement(0, 200);
+		drawFinishElement(0, 200);
 	}
 	
-	function drawHorizontalTrackElement(x, y){
+	function drawHorizontalTrackElement(x, y, roadColor){
 		drawRectangle(x, y, 200, 200, "black");
 		drawRectangle(x, y + 15, 200, 170, "green");
-		drawRectangle(x, y + 50, 200, 100, "grey");
+		drawRectangle(x, y + 50, 200, 100, roadColor);
 	}
 	
 	function drawVerticalTrackElement(x, y){
@@ -30,6 +30,29 @@
 		drawCirkel(x, y, 150, angle, "grey");
 		drawCirkel(x, y, 50, angle, "green");
 		drawCirkel(x, y, 15, angle, "black");
+	}
+	
+	function drawFinishElement(x, y){
+		drawVerticalTrackElement(x, y);
+		drawRectangle(x + 50, y, 25, 50, "black");
+		drawRectangle(x + 75, y, 25, 50, "white");
+		drawRectangle(x + 100, y, 25, 50, "black");
+		drawRectangle(x + 125, y, 25, 50, "white");
+		
+		drawRectangle(x + 50, y + 50, 25, 50, "white");
+		drawRectangle(x + 75, y + 50, 25, 50, "black");
+		drawRectangle(x + 100, y + 50, 25, 50, "white");
+		drawRectangle(x + 125, y + 50, 25, 50, "black");
+		
+		drawRectangle(x + 50, y + 100, 25, 50, "black");
+		drawRectangle(x + 75, y + 100, 25, 50, "white");
+		drawRectangle(x + 100, y + 100, 25, 50, "black");
+		drawRectangle(x + 125, y + 100, 25, 50, "white");
+		
+		drawRectangle(x + 50, y + 150, 25, 50, "white");
+		drawRectangle(x + 75, y + 150, 25, 50, "black");
+		drawRectangle(x + 100, y + 150, 25, 50, "white");
+		drawRectangle(x + 125, y + 150, 25, 50, "black");
 	}
 	
 	function drawRectangle(x, y, w, h, style){
